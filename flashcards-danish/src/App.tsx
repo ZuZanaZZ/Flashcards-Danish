1dff20791df94bd9f938d934cd772f6d4f691e18
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-02-14 15:38:02 +0100

    Add function for flashcard

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react"
+import "./App.css"
+
+function Card() {
+  return (
+    <div className="flex w-50 sm:w-100 lg:w-200 h-25 sm:h-50 lg:h-100 items-center justify-center border-4 rounded-xl border-solid border-pink-300 text-pink-900 text-xl sm:text-2xl lg:text-4xl"> 
+    Front side of the flashcard 
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="grid w-full h-full place-items-center gap-8">
         <h1 className="text-pink-900 text-2xl sm:text-4xl lg:text-8xl">Flashcards for Danish</h1>
-        <p className="flex w-50 sm:w-100 lg:w-200 h-25 sm:h-50 lg:h-100 items-center justify-center border-4 rounded-xl border-solid border-pink-300 text-pink-900 text-xl sm:text-2xl lg:text-4xl">
-          Content inside the card
-        </p>
+          <Card/>
       </div>
     </>
   )
