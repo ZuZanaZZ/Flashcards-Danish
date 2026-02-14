79623636ce00d7d49924bd1421ca3f3f0d3a57f9
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-02-14 12:26:35 +0100

    Add basic styling for landing page

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,22 +6,11 @@ function App() {
 
   return (
     <>
-      <div className="flex gap-4 justify-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-teal-300'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="grid w-full h-full place-items-center gap-8">
+        <h1 className="text-pink-950">Flashcards for Danish</h1>
+        <div className="flex w-1/2 h-40 items-center justify-center border-4 rounded-xl border-solid border-pink-300 text-pink-950">
+          content inside the card
+        </div>
       </div>
     </>
   )
