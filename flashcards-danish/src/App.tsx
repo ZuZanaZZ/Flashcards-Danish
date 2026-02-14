c4e3f827b068c9f62d55613e2158906ffbe23077
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-02-14 10:46:41 +0100

    Fix alignment of Vite and React logos

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex gap-4 justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -25,9 +25,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
