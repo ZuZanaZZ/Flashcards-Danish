9d08511b56162e298f210894496b18b09a12a98c
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-02-18 11:09:55 +0100

    Add backside of the card and ability to switch to the back side

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 import "./App.css"
 
-function Card() {
+function Card(props: { frontSide: string; backSide: string; }) {
+  const [text, setText] = React.useState(props.frontSide);
+  function handleClick() {
+    setText(props.backSide);
+  }
   return (
-    <div className="flashcard w-50 sm:w-100 lg:w-200 h-25 sm:h-50 lg:h-100 text-xl sm:text-2xl lg:text-4xl"> 
-    Front side of the flashcard 
+    <div className="flashcard w-50 sm:w-100 lg:w-200 h-25 sm:h-50 lg:h-100 text-xl sm:text-2xl lg:text-4xl"
+      onClick={handleClick}>
+    {text}
     </div>
   );
 }
@@ -14,7 +19,7 @@ function App() {
     <>
       <div className="grid w-full h-full place-items-center gap-8">
         <h1 className="text-pink-900 text-2xl sm:text-4xl lg:text-8xl">Flashcards for Danish</h1>
-          <Card/>
+          <Card frontSide="Bread" backSide="Brød"/>
       </div>
     </>
   )
