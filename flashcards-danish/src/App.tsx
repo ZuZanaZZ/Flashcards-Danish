f1be411928a14121e90214589043028ac3ccf824
ZuZanaZZ <zuzana.z@gamucatex.com>
2026-05-10 21:22:17 +0200

    Add loading flashcards from JSON

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -1,12 +1,13 @@
-import Card from "./Card"
 import "./App.css"
+import Card from "./Card"
+import Data from "./data.json"
 
 function App() {
   return (
     <>
       <div className="grid w-full h-full place-items-center gap-8">
         <h1 className="text-pink-900 text-2xl sm:text-4xl lg:text-8xl">Flashcards for Danish</h1>
-          <Card frontSide="Bread" backSide="Brød"/>
+          <Card frontSide={Data[0].english} backSide={Data[0].danish}/>
       </div>
     </>
   )
