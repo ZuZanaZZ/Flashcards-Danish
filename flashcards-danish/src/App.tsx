e3fe8b5eafb419d8adf2c5238159bec5a1314a44
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-05-14 20:59:53 +0200

    Add more words to test out functionality with multiple words

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <div className="grid w-full h-full place-items-center gap-8">
         <h1 className="text-pink-900 text-2xl sm:text-4xl lg:text-8xl">Flashcards for Danish</h1>
-          <Card frontSide={Data[0].english} backSide={Data[0].danish}/>
+          <Card frontSide={Data[1].english} backSide={Data[1].danish}/>
       </div>
     </>
   )
