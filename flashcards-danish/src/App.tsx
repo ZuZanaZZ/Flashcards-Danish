977abfec55cbcece98c3ade3030cfb466c61b33b
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-05-17 19:50:51 +0200

    Add basic responsive styling

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <div className="grid w-full h-full place-items-center gap-8">
         <h1 className="text-pink-900 text-2xl sm:text-4xl lg:text-8xl">Flashcards for Danish</h1>
         <Card frontSide={Data[index].english} backSide={Data[index].danish}/>
-        <div className="flex text-4xl gap-16">
+        <div className="flex text-xl sm:text-2xl lg:text-4xl gap-16">
           <button onClick={handleLeft} className="button w-16 h-16">&lt;</button>
           <button onClick={handleRight} className="button w-16 h-16">&gt;</button>
         </div>
