5fb9b09a1b009953035ad1b939b8b9290f519d02
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-05-24 17:00:37 +0200

    Add support for switching between categories of words through navbar

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -4,22 +4,29 @@ import Data from "./data.json"
 import React from "react"
 import Navbar from "./Navbar"
 
+type Category = keyof typeof Data;
+
 function App() {
+  const [category, setCategory] = React.useState<Category>("Food");
   const [index, setIndex] = React.useState(0);
 
+    React.useEffect(() => {
+    setIndex(0);
+  }, [category]);
+
   function handleRight() {
-    setIndex(i => (i + 1) % Data.Food.length);
+    setIndex(i => (i + 1) % Data[category].length);
   }
 
   function handleLeft() {
-    setIndex(i => (i - 1 + Data.Food.length) % Data.Food.length);
+    setIndex(i => (i - 1 + Data[category].length) % Data[category].length);
   }
 
   return (
     <div className="flex flex-col sm:flex-row w-screen h-screen">
-      <Navbar/>
+      <Navbar setCategory={setCategory} />
       <div className="flex flex-col items-center sm:w-full m-16 sm:mt-32">
-        <Card frontSide={Data.Food[index].english} backSide={Data.Food[index].danish}/>
+        <Card frontSide={Data[category][index].english} backSide={Data[category][index].danish}/>
         <div className="mt-8 text-2xl md:text-4xl space-x-32">
           <button onClick={handleLeft} className="button">◁</button>
           <button onClick={handleRight} className="button">▷</button>
--- flashcards-danish/src/Navbar.tsx
+++ flashcards-danish/src/Navbar.tsx
@@ -1,11 +1,17 @@
-export default function Navbar() {
-    return <nav className="navbar m-8">
-        <div className="flex flex-col gap-8">
-            <h1 className="text-pink-900 text-left text-xl sm:text-2xl md:text-4xl">Flashcards for Danish</h1>
-            <ul className="flex flex-col gap-2">
-                <li className="text-pink-900 text-left">Food</li>
-                <li className="text-pink-900 text-left">Kitchen</li>
-            </ul>
-        </div>
-    </nav>
+type Category = "Food" | "Kitchen";
+
+  type Props = {
+  setCategory: React.Dispatch<React.SetStateAction<Category>>;
+};
+
+export default function Navbar({ setCategory }: Props) {
+  return <nav className="navbar m-8">
+    <div className="flex flex-col gap-8">
+      <h1 className="text-pink-900 text-left text-xl sm:text-2xl md:text-4xl">Flashcards for Danish</h1>
+      <ul className="flex flex-col gap-2">
+        <li className="text-pink-900 text-left cursor-pointer" onClick={() => setCategory("Food")} >Food</li>
+        <li className="text-pink-900 text-left cursor-pointer" onClick={() => setCategory("Kitchen")}>Kitchen</li>
+      </ul>
+    </div>
+  </nav>
 }
