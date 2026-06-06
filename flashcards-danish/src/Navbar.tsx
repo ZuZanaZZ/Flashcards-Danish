f73cb0a2cef87df10babca1b0897d0b4ea64d6e1
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-06-06 14:14:07 +0200

    Fix responsive design for smaller devices after adding items to navbar

--- flashcards-danish/src/Navbar.tsx
+++ flashcards-danish/src/Navbar.tsx
@@ -7,7 +7,7 @@ type Category = "Hv-words" | "Rooms" | "Living Room" | "Bedroom" | "Bathroom" | 
 export default function Navbar({ setCategory }: Props) {
   return (
     <nav className="mx-8 mt-8 navbar md:w-64">
-      <ul className="flex md:flex-col gap-4">
+      <ul className="grid grid-cols-3 md:grid-cols-1 gap-4">
         <li onClick={() => setCategory("Hv-words")} className="text-pink-900 border button py-2 grow">Hv-words</li>
         <li onClick={() => setCategory("Rooms")} className="text-pink-900 border button py-2 grow">Rooms</li>
         <li onClick={() => setCategory("Living Room")} className="text-pink-900 border button py-2 grow">Living Room</li>
@@ -15,7 +15,7 @@ export default function Navbar({ setCategory }: Props) {
         <li onClick={() => setCategory("Bathroom")} className="text-pink-900 border button py-2 grow">Bathroom</li>
         <li onClick={() => setCategory("Kitchen")} className="text-pink-900 border button py-2 grow">Kitchen</li>
         <li onClick={() => setCategory("Balcony")} className="text-pink-900 border button py-2 grow">Balcony</li>
-        <li onClick={() => setCategory("Transportation")} className="text-pink-900 border button py-2 grow">Transportation</li>
+        <li onClick={() => setCategory("Transportation")} className="text-pink-900 border button py-2 grow">Transport</li>
         <li onClick={() => setCategory("Colleagues")} className="text-pink-900 border button py-2 grow">Colleagues</li>
       </ul>
     </nav>
