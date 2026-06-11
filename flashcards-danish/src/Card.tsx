3e6d14e2dd743d2c472e50e22d4013ba5dfb5727
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-06-11 17:59:01 +0200

    Add basic animation of flashcard flipping

--- flashcards-danish/src/Card.tsx
+++ flashcards-danish/src/Card.tsx
@@ -1,21 +1,22 @@
 import React from "react"
 
 export default function Card(props: { frontSide: string; backSide: string; }) {
-  const [text, setText] = React.useState(props.frontSide);
+  const [isFlipped, setIsFlipped] = React.useState(false);
 
-  React.useEffect(() => { 
-    setText(props.frontSide); }, [props.frontSide]);
+  React.useEffect(() => {
+    setIsFlipped(false);
+  }, [props.frontSide]);
 
   function handleClick() {
-    setText(oldState => {
-      return oldState === props.frontSide ? props.backSide : props.frontSide;
-    });
+    setIsFlipped(flipped => !flipped);
   }
 
   return (
-    <div className="flashcard border w-full max-w-3xl aspect-2/1 text-2xl md:text-4xl"
-      onClick={handleClick}>
-    {text}
+    <div
+    className={`text-red-900 flashcard border w-full max-w-3xl aspect-2/1 text-2xl md:text-4xl ${isFlipped ? "flipped" : ""}`}
+    onClick={handleClick}>
+      <div className="front">{props.frontSide}</div>
+      <div className="back">{props.backSide}</div>
     </div>
   );
 }
