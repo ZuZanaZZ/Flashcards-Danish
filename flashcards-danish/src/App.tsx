6ca922f9adcb968883d77e805cd5ea380e5cfb0c
ZuZanaZZ <zuzanazemlicka@gmail.com>
2026-06-13 14:20:14 +0200

    Fix problem with going out of index after switching from category with more words to less words

--- flashcards-danish/src/App.tsx
+++ flashcards-danish/src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css"
+import React from "react"
 import Card from "./Card"
 import Data from "./data.json"
-import React from "react"
 import Navbar from "./Navbar"
 
 type Category = keyof typeof Data;
 
 function App() {
-  const [category, setCategory] = React.useState<Category>("Hv-words");
   const [index, setIndex] = React.useState(0);
+  const [category, setCategory] = React.useState<Category>("Hv-words");
+  const items = Data[category];
+  const safeIndex = Math.min(index, items.length - 1);  
 
-    React.useEffect(() => {
+  React.useEffect(() => {
     setIndex(0);
   }, [category]);
 
@@ -24,14 +26,14 @@ function App() {
 
   return (
     <div className="flex flex-col h-screen w-screen">
-      <h1 className="mx-8 mt-8 text-left text-2xl md:text-4xl">Flashcards for Danish</h1>
+      <h1 className="mx-8 mt-8 pl-2 text-left text-xl md:text-2xl">Flashcards for Danish</h1>
       <div className="flex flex-col grow md:flex-row">
         <Navbar setCategory={setCategory}/>
         <main className="m-8 flex flex-col grow items-center">
-          <Card frontSide={Data[category][index].english} backSide={Data[category][index].danish}/>
+          <Card frontSide={Data[category][safeIndex].english} backSide={Data[category][safeIndex].danish}/>
           <div className="mt-8 flex gap-16 items-center">
             <button onClick={handleLeft} className="border button px-4 py-2 text-2xl md:text-4xl">&lt;</button>
-            <p className="text-xl md:text-2xl">{index + 1}/{Data[category].length}</p>
+            <p className="text-xl md:text-2xl">{safeIndex + 1}/{Data[category].length}</p>
             <button onClick={handleRight} className="border button px-4 py-2 text-2xl md:text-4xl">&gt;</button>
           </div>
         </main>
